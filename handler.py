6fba7e19393af8ba948da8b585e8969e3abf8797
snazzzy <jatraner@gmail.com>
2020-05-17 11:10:13 -0700

    added score json structure

--- handler.py
+++ handler.py
@@ -1,11 +1,43 @@
-import flask
-
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+from flask import Flask, jsonify, request
 
 
-@app.route('/', methods=['GET'])
-def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+app = Flask(__name__)
 
-app.run()
+tasks = [
+    {
+        'id': 1,
+        'value': u'test'
+    },
+    {
+        'id': 2,
+        'value': u'score'
+    }
+]
+
+#Need to implement PUT. The Handler will pass JWT here.
+@app.route('/<int:task_id>', methods=['GET', 'PUT'])
+def update_task(task_id):
+
+    if request.method == 'GET':
+        return jsonify({'tasks': tasks})
+    
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    
+    tasks[0]['value'] = request.json.get('value', tasks[0]['value'])
+    return jsonify({'tasks': tasks[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+#Query Command
+# curl -i -H "Content-Type: application/json" -X PUT -d "{\"JWT\":\"VALUE\"}" http://localhost:5000/1
+
+# curl -i http://localhost:5000/1
